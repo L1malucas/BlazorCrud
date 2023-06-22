64dfb3eb433d408a2fed8e1b724fcb455856f0b2
lucas lima <lucaslj@ufba.br>
2023-06-21 21:38:15 -0300

    models setup

--- BlazorCrud/Server/data/ApiDbContext.cs
+++ BlazorCrud/Server/data/ApiDbContext.cs
@@ -1,11 +1,13 @@
+using BlazorCrud.Shared.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorCrud.Server.Data;
 
-public class ApiDbContext : DbContext 
+public class ApiDbContext : DbContext
 {
-    public ApiDbContext (DbContextOptions<ApiDbContext> options) : base(options)  
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
     {
 
     }
-}	
+    public DbSet<Driver> Drivers { get; set; }
+}
---
+++ BlazorCrud/Shared/Models/Driver.cs
@@ -0,0 +1,9 @@
+namespace BlazorCrud.Shared.Models;
+
+public class Driver
+{
+    public int Id { get; set; }
+    public String Name { get; set; } = string.Empty;
+    public String RacingNb { get; set; } = string.Empty;
+    public String Team { get; set; } = string.Empty;
+}
